b30e897fef1103d82ac82ecbb87d795c92b538b6
Momtchil Momtchev <momtchil@momtchev.com>
2020-07-09 19:57:34 +0200

    remove useless arrow function

--- readcsv-event-stream.js
+++ readcsv-event-stream.js
@@ -5,7 +5,5 @@ const parseline = require('./parseline');
 
 const rs = fs.createReadStream('allCountries.txt');
 console.time(__filename);
-rs.pipe(es.split()).pipe(es.through((line) => {
-	parseline(line);
-}));
+rs.pipe(es.split()).pipe(es.through(parseline));
 rs.on('close', () => console.timeEnd(__filename));
